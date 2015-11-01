993cc00f71b8624c3c00c1bec5aeaa7d3823a045
ihortom <inet@tomilenko.tk>
2015-10-31 21:59:53 -0600

    Added pagination with 'foundation' markup

---
+++ taxonomy-pweb_post_type-news.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Custom taxonomy
+ *
+ * @package ProperWeb
+ * @subpackage ProperFramework
+ * @since ProperFramework 1.0
+ * @uses Meta Box: 'rwmb_meta_boxes' @see http://metabox.io/
+ * @uses Single-Choice Post Taxonomy: 'pweb_post_type' @see ../../mu-plugins/pweb_single_choice_tax.php
+ */
+?>
+
+<?php get_header(); ?>
+
+<div class="row row-with-sidebar">
+    <div class="cell">
+<?php if(have_posts()): while(have_posts()): the_post(); ?>
+        <div class="article article-with-sidebar box full-width">
+            <h2 class="title"><?php the_title(); ?></h2>
+            <time datetime="<?php the_date(); ?>" class="post-date">
+                    <?php the_time( get_option( 'date_format' ) ); ?>
+            </time>
+            </time>
+            <p class="line"></p>
+            <?php 
+                if ( has_post_thumbnail() ) {
+                    the_post_thumbnail( 'thumbnail', array( 'class' => 'thumbnail alignleft') );
+                }
+                the_content(); 
+            ?>
+        </div>
+<?php endwhile; else : ?>
+        <div class="article article-with-sidebar box full-width er404">
+            <h2 class="title">No post submitted</h2>
+            <h3>Sorry, this category contains no post</h3>
+            <p>The corresponding content will be added later on.</p>
+        </div>
+<?php endif; ?>
+    </div>
+    <div id="aside" class="right-sidebar">
+        <div  class="box with-pads">
+            <ul id="aside-sidebar" class="sidebar">
+                    <?php dynamic_sidebar( 'Aside' ); ?>
+            </ul>
+        </div>
+    </div>
+</div>	
+<?php get_footer(); ?>
